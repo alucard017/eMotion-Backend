97308df1b07315ee00bae6a4fce9858fce76dc34
Apurba Sundar Nayak <apurbasundar2002@gmail.com>
2025-06-07 18:48:50 +0530

    added rate limiting on gateway server

--- src/gateway-server/src/server.ts
+++ src/gateway-server/src/server.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import expressProxy from "express-http-proxy";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+
 const app = express();
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -9,8 +12,25 @@ app.use(
   })
 );
 
-app.use("/api/user", expressProxy("http://localhost:8001"));
-app.use("/api/captain", expressProxy("http://localhost:8002"));
+const userLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests to user API, please try again later.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const captainLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests to captain API, please try again later.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use("/api/user", userLimiter, expressProxy("http://localhost:8001"));
+app.use("/api/captain", captainLimiter, expressProxy("http://localhost:8002"));
+
 app.use("/api/ride", expressProxy("http://localhost:8003"));
 
 app.listen(8000, () => {
